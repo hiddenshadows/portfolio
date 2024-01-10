28a1243fdb43b6fe85f7fec9b8ef398635c82c70
Edgar Munoz <munozedgar510@gmail.com>
2024-01-09 20:31:19 -0800

    IMage carousel added

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,43 +1,117 @@
 'use client';
 
-import React from "react";
-
-const projects = [
-  {
-    name: 'Slack Clone',
-    description: `A full-stack clone of Slack with a server side rendered landing page, 
-      login page, signup page, and home page. In the homepage you can see you workspaces and 
-      the different channels in the workspace as well as messages. you can also send messages and
-      create new workspaces and/or channels. Created using React, Next.js, Node.js, GraphQL, 
-      PostgresSQL and MUI`,
-    img: 'images/SlackLandingPage.png',
-    link: 'https://tiered-web-app-fe-sn7yhk7rkq-uw.a.run.app/',
-    git: '#'
-  },
-  {
-    name: 'InterNews',
-    description: `A group project that shows news throughout the whole world.
-      It has an interactive map and in which you can click on any country and 
-      see the news from that country. You can also create an account and bookmark news
-      articles. It was made with HTML, CSS, Javascript and Firebase for backend and database.`,
-    img: 'images/InternewsHome.png',
-    link: 'https://internews-365121.web.app/index.html',
-    git: '#'
-  },
-  {
-    name: `Bizzare`,
-    description: `A group project that is a marketplace similar to Offerup or Craigslist.
-      You can view items while being logged out and when you login you can upload your own
-      things to sell. Built using the micro services architechture. Created using React, Next.js, 
-      Node.js, GraphQL, PostgresSQL and MUI`,
-    img: 'images/BIZarreHome.png',
-    link: '#',
-    git: '#'
-  },
-
-]
+import React, { useState } from "react";
 
 export default function Projects() {
+  const [slack, changeSlack] = useState(0)
+  const [inter, changeInter] = useState(0)
+  const [biz, changeBiz] = useState(0)
+
+
+  // const handleSlack = () => {
+  //   if(slack === 0){
+  //     changeSlack(1)
+  //   } else {
+  //     changeSlack(0)
+  //   }
+  // }
+
+  // const handleInter = () => {
+  //   if(inter !== projects[1].img.length - 1){
+  //     changeInter(inter + 1)
+  //   } else {
+  //     changeInter(0)
+  //   }
+  // }
+
+  // const handleBiz = () => {
+  //   if(biz !== projects[2].img.length - 1){
+  //     changeBiz(biz + 1)
+  //   } else {
+  //     changeBiz(0)
+  //   }
+  // }
+
+  const projects = [
+    {
+      name: 'Slack Clone',
+      description: `A full-stack clone of Slack with a server side rendered landing page, 
+        login page, signup page, and home page. In the homepage you can see you workspaces and 
+        the different channels in the workspace as well as messages. you can also send messages and
+        create new workspaces and/or channels. Created using React, Next.js, Node.js, GraphQL, 
+        PostgresSQL and MUI`,
+      img: ['images/SlackLandingPage.png', 'images/SlackLogin.png'],
+      link: 'https://tiered-web-app-fe-sn7yhk7rkq-uw.a.run.app/',
+      git: '#',
+      stateVar: slack,
+      next() {
+        if(slack === 0) {
+          changeSlack(1)
+        } else {
+          changeSlack(0)
+        }
+      },
+      prev() {
+        if(slack === 0) {
+          changeSlack(1)
+        } else {
+          changeSlack(0)
+        }
+      }
+    },
+    {
+      name: 'InterNews',
+      description: `A group project that shows news throughout the whole world.
+        It has an interactive map and in which you can click on any country and 
+        see the news from that country. You can also create an account and bookmark news
+        articles. It was made with HTML, CSS, Javascript and Firebase for backend and database.`,
+      img: ['images/InternewsHome.png', 'images/InternewsUS.png'],
+      link: 'https://internews-365121.web.app/index.html',
+      git: '#',
+      stateVar: inter,
+      next() {
+        if(inter !== projects[1].img.length - 1){
+          changeInter(inter + 1)
+        } else {
+          changeInter(0)
+        }
+      },
+      prev() {
+        if(inter === 0) {
+          changeInter(projects[1].img.length - 1)
+        } else {
+          changeInter(inter - 1)
+        }
+      }
+    },
+    {
+      name: `Bizzare`,
+      description: `A group project that is a marketplace similar to Offerup or Craigslist.
+        You can view items while being logged out and when you login you can upload your own
+        things to sell. Built using the micro services architechture. Created using React, Next.js, 
+        Node.js, GraphQL, PostgresSQL and MUI`,
+      img: ['images/BIZarreHome.png', 'images/BIZarreCreate.png', 'images/BIZarreI18EditPost.png', 'images/BIZarreInbox.png', 'images/BIZarreItem.png', 'images/BIZarreSearch.png'],
+      link: '#',
+      git: '#',
+      stateVar: biz,
+      next() {
+        if(biz !== projects[2].img.length - 1){
+          changeBiz(biz + 1)
+        } else {
+          changeBiz(0)
+        }
+      },
+      prev() {
+        if(biz === 0) {
+          changeBiz(projects[2].img.length - 1)
+        } else {
+          changeBiz(biz - 1)
+        }
+      }
+    },
+  
+  ]
+
   return (
     <div className="container mx-auto px-8">
       <h1 className="text-4xl font-bold tracking-tight text-white-900 sm:text-4xl text-center p-2">
@@ -46,7 +120,16 @@ export default function Projects() {
       {projects.map( proj => 
         <div className="card lg:card-side bg-base-200 shadow-xl my-8">
           <figure>
-            <img src={proj.img} alt="Screenshot"/>
+            {/* <img src={proj.img} alt="Screenshot"/> */}
+            <div className="carousel w-full">
+              <div id={`slide${Math.floor(Math.random() * 10)}`} className="carousel-item relative w-full">
+                <img src={proj.img[proj.stateVar]} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a onClick={proj.prev} className="btn btn-circle">❮</a> 
+                  <a onClick={proj.next} className="btn btn-circle">❯</a>
+                </div>
+              </div> 
+            </div>
           </figure>
           <div className="card-body">
             <h2 className="card-title">{proj.name}</h2>
